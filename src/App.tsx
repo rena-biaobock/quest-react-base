67daa8b7c03949b30e137d99b75611d4dc152273
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-12-26 14:33:07 -0300

    :sparkles: feat: add component to render a buton with label as prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
+import { Button } from "./components/Button";
 import { ColoredParagraph } from "./components/ColoredParagraph";
 
 function App() {
   return (
-    <ColoredParagraph
-      paragraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut congue ex eu iaculis sollicitudin. Nunc consequat non risus id faucibus. Mauris ullamcorper, libero facilisis tincidunt ullamcorper, sapien lacus congue massa, id suscipit ipsum ante ac nunc."
-      textColor="#FF1189"
-    />
+    <>
+      <ColoredParagraph
+        paragraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut congue ex eu iaculis sollicitudin. Nunc consequat non risus id faucibus. Mauris ullamcorper, libero facilisis tincidunt ullamcorper, sapien lacus congue massa, id suscipit ipsum ante ac nunc."
+        textColor="#FF1189"
+      />
+
+      <Button label="LABEL" />
+    </>
   );
 }
 
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+type Props = {
+  label: string;
+};
+
+export const Button = ({ label }: Props) => {
+  return <button>{label}</button>;
+};
