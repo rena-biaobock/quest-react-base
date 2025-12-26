abbb494e731cf4f0ec27b08246ce5c6ad480a1f7
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-12-26 14:08:54 -0300

    :sparkles: feat: add component to render a paragraph with text and color as props

---
+++ src/components/ColoredParagraph/index.tsx
@@ -0,0 +1,8 @@
+type Props = {
+  paragraphText: string;
+  textColor?: string;
+};
+
+export const ColoredParagraph = ({ paragraphText, textColor }: Props) => {
+  return <p style={{ color: textColor }}>{paragraphText}</p>;
+};
